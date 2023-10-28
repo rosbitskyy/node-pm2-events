19a6f03ced9cd1574297df7a7e227f6ebb5ba997
Руслан Розбицкий <rosbitskyy@gmail.com>
2023-10-29 00:32:22 +0300

    ☕️ https in test

--- tests/eventbus-cluster-node.js.v.20.js
+++ tests/eventbus-cluster-node.js.v.20.js
@@ -7,7 +7,7 @@
 const EventBus = require('../src/EventBus');
 const {sleep} = require('../src/EventBus/utils')
 const cluster = require('cluster');
-const http = require('http');
+const https = require('https');
 const {availableParallelism} = require('os');
 
 const numCPUs = Math.max(2, Math.min(2, availableParallelism()));
@@ -162,7 +162,7 @@ const start = async () => {
     } else {
         // Workers can share any TCP connection
         // In this case it is an HTTP server
-        http.createServer((req, res) => {
+        https.createServer((req, res) => {
             res.writeHead(200);
             res.end('hello world\n');
         }).listen(8080);
